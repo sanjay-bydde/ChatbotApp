f682901521cb32bbe51ee638915b7f8e670831b6
sanjay <sanjaybudde27@gmail.com>
2025-04-17 15:57:08 -0500

    First Commit

---
+++ chatbot-service/src/test/java/com/ChatGPT/chatbot_service/ChatbotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChatGPT.chatbot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
